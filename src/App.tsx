852a2a217e11a42c11a6ae01c16b315fd5ddd371
Mauricio Mityo Hidani <mauricio.hidani.work@gmail.com>
2023-12-01 09:04:18 -0300

    feat: add global theme provider and init presentation

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
+import Presentation from './Components/Presentation/Presentation';
+import { ThemeProvider } from 'styled-components';
+import GlobalStyle from './Themes/GlobalStyle';
+import Theme from './Themes/Theme';
 
 function App() {
   return (
-    <div className="App">
-      Salamander
-    </div>
+    <ThemeProvider theme={Theme}>
+      <GlobalStyle />
+      <section>
+        <Presentation />
+      </section>
+    </ThemeProvider>
   );
 }
 
