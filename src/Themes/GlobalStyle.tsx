2dfae8ae2af5eae60fbe51de0b51c1780ec338b9
Mauricio Mityo Hidani <mauricio.hidani.work@gmail.com>
2023-12-01 09:02:37 -0300

    feat: init themes settings

---
+++ src/Themes/GlobalStyle.tsx
@@ -0,0 +1,31 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+  
+  @font-face {
+    font-family: 'JetBrains Mono';
+    src: url('./caminho/para/sua/fonte/JetBrainsMono-Regular.woff2') format('woff2');
+    /* Adicione outros formatos de fonte conforme necessário */
+    font-weight: 400;
+    font-style: normal;
+  }
+  
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-weight: normal;
+    font-family: 'JetBrains Mono', monospace;
+    font-size: 12pt;
+  }
+
+  a {
+    text-decoration: none;
+    color: ${props => props.theme.colors.link};
+    transition: .2s ease-out;
+
+    &:hover { color: ${props => props.theme.colors.linkHover}; }
+  }
+`;
+
+export default GlobalStyle;
---
+++ src/Themes/Theme.tsx
@@ -0,0 +1,14 @@
+
+
+const Theme = {
+  stdFontSize: "12pt",
+  occupationSize: "3rem",
+
+  colors: {
+    textLight: "#7f92d1",
+    link: "#8ADAB2",
+    linkHover: "#D0F288",
+  },
+};
+
+export default Theme;
