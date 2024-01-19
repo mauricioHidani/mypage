31b299bd5c101a4c9c348b902022fbdd3a511dac
Mauricio Mityo Hidani <mauricio.hidani.work@gmail.com>
2024-01-19 20:32:15 -0300

    feat: footer component

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,3 +1,4 @@
+import { data as footer } from "@/data/footerData";
 
 export default function Footer(
     {
@@ -5,8 +6,50 @@ export default function Footer(
     }
 ) {
     return(
-        <div>
-            footer
+        <div
+            className="
+                flex 
+                flex-col 
+                items-center 
+                justify-center 
+                py-5 
+                text-center 
+                bg-color-1th 
+                text-sm 
+                text-color-normal  
+            "
+        >
+            <div
+                className="
+                    flex 
+                    flex-row 
+                    gap-1 
+                "
+            >
+                <span>{ footer.since }</span>
+                <span>&#45;</span>
+                <span>{ footer.local }</span>
+            </div>
+            <div
+                className="
+                    flex 
+                    gap-2
+                    text-green-400  
+                "
+            >
+                <span>{ footer.version }</span>
+                <span>version</span>
+            </div>
+            <div
+                className="
+                    flex 
+                    gap-1 
+                    text-green-900 
+                "
+            >
+                <span>created by</span>
+                <span>{ footer.author }</span>
+            </div>
         </div>
     );
 }
--- src/data/footerData.tsx
+++ src/data/footerData.tsx
@@ -1,3 +1,7 @@
 
 export const data = {
+    since: "November 20, 2023", 
+    local: "São Paulo, Brazil", 
+    version: "1.2.0", 
+    author: "Mauricio Mityo Hidani" 
 };
