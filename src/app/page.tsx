cfd7e861c2effdcb0f4797c0901b7d554833dc99
Mauricio Mityo Hidani <mauricio.hidani.work@gmail.com>
2024-01-20 10:16:54 -0300

    feat: change discount navbar full dark component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,15 @@
 
 import DiscountNavbarFullDark from "@/components/discountNavbarFullDark";
+import Presentation from "@/components/presentation";
 import Image from "next/image";
+
+import { data as person } from "@/data/personData";
 
 export default function Home() {
     return (
         <main>
             <section>
-                <DiscountNavbarFullDark>
-                    presentation
-                </DiscountNavbarFullDark>
+                <Presentation person={ person } />
                 section
             </section>
         </main>
