ba77aba375dae60395118f7c99a6382915d337b2
Mauricio Mityo Hidani <mauricio.hidani.work@gmail.com>
2024-01-20 10:55:47 -0300

    feat: circular button

---
+++ src/components/circularLink.tsx
@@ -0,0 +1,44 @@
+import Link from "next/link";
+import { ReactNode } from "react";
+
+export default function CircularLink(
+    {
+        path, 
+        title, 
+        target, 
+        size, 
+        children  
+    }:
+    {
+        path: string, 
+        title: string, 
+        target?: string, 
+        size: string, 
+        children: ReactNode | null 
+    }
+) {
+    return(
+        <Link 
+            href={ path }
+            title={ title }
+            target={ target } 
+        >
+            <div
+                className={`
+                    flex 
+                    items-center 
+                    justify-center 
+                    ${ "w-"+size } 
+                    ${ "h-"+size } 
+                    p-2 
+                    rounded-full 
+                    bg-color-normal
+                    hover:bg-color-hover 
+                    active:bg-color-active 
+            `}
+            >
+                { children }
+            </div>
+        </Link>
+    );
+}
