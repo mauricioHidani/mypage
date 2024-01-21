25ed663f0a5952e6ae9e7e5ec98e044d50ff10d0
Mauricio Mityo Hidani <mauricio.hidani.work@gmail.com>
2024-01-21 14:04:40 -0300

    fix: string empty check

--- src/components/smallItemByRight.tsx
+++ src/components/smallItemByRight.tsx
@@ -19,8 +19,8 @@ export default function SmallItemByRight(
                 items-center
                 justify-end
                 right-0
-                ${mobMarginX !== null ? "mx-["+mobMarginX+"]" : "mx-10"}
-                ${dekMarginX !== null ? "md:mx-["+dekMarginX+"]" : "md:mx-28"}
+                ${!mobMarginX ? "mx-["+mobMarginX+"]" : "mx-10"}
+                ${!dekMarginX ? "md:mx-["+dekMarginX+"]" : "md:mx-28"}
             `}
         >
             { children }
