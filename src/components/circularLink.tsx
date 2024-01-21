834dd4698775740e9832482d4a967db563e159ca
Mauricio Mityo Hidani <mauricio.hidani.work@gmail.com>
2024-01-21 14:11:03 -0300

    refact> text color white

--- src/components/circularLink.tsx
+++ src/components/circularLink.tsx
@@ -35,6 +35,7 @@ export default function CircularLink(
                     bg-color-normal
                     hover:bg-color-hover 
                     active:bg-color-active 
+                    text-white
             `}
             >
                 { children }
