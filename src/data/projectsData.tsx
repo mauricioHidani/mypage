c47c7c45558245caf89abb8d62e6ec6f2b7f0ac4
Mauricio Mityo Hidani <mauricio.hidani.work@gmail.com>
2024-01-21 14:25:42 -0300

    test: projects test data

---
+++ src/data/projectsData.tsx
@@ -0,0 +1,24 @@
+
+export const data = [
+    {
+        id: "7003b6b8-0e2c-4f77-8c67-e8ee13422c1a",
+        title: "Projeto Olivy",
+        snapshotContent: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima necessitatibus, unde blanditiis magnam dolor, rem voluptatibus ad asperiores neque velit voluptas et reiciendis tempora libero debitis reprehenderit repellendus dolorem officia?",
+        imageUrl: "/landscape.jpg",
+        path: "/"
+    },
+    {
+        id: "bd11a6e7-d520-42c5-8474-322b7d8104a7",
+        title: "Projeto Mora",
+        snapshotContent: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima necessitatibus, unde blanditiis magnam dolor, rem voluptatibus ad asperiores neque velit voluptas et reiciendis tempora libero debitis reprehenderit repellendus dolorem officia?",
+        imageUrl: "/landscape.jpg",
+        path: "/"
+    },
+    {
+        id: "efa5ac2f-2b12-485a-b679-4750b1c3c223",
+        title: "Projeto Dibis",
+        snapshotContent: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima necessitatibus, unde blanditiis magnam dolor, rem voluptatibus ad asperiores neque velit voluptas et reiciendis tempora libero debitis reprehenderit repellendus dolorem officia?",
+        imageUrl: "/landscape.jpg",
+        path: "/"
+    },
+];
